65ed75f1db994f5d11c4cce9e6d18f918d9a79f2
Elias <ekasa123@gmail.com>
2026-09-19 18:45:51 +0300

    New Project With Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('first-app');
-}
+export class App {}
